442f3b11a9ebc96483ec8077d745ef12225125a9
Rohan S Bhoi <s3764268@student.rmit.edu.au>
2021-11-08 17:08:09 +1100

    Incorporating useReducer
    The 'Reducer Function' takes in the 'Current  State' and returns the 'New State'.

--- 1.React.js Essential Training/3.React State in a Component Tree/react-essentials/src/App.js
+++ 1.React.js Essential Training/3.React State in a Component Tree/react-essentials/src/App.js
@@ -1,37 +1,23 @@
 import './App.css';
-import React, { useState, useEffect } from "react";
+import React, {useReducer} from "react";
 
 function App() {
-  const [emotion, setEmotion] = useState("happy");
-  const [secondary, setSecondary] = useState("tired");
-
-  useEffect(() => {
-    console.log(`Its ${emotion} around here!`);
-  }, [emotion]);
-
-  useEffect(() => {
-    console.log(`Its ${secondary} around here!`);
-  }, [secondary]);
+  const [cheked, toggle] = useReducer(
+    (cheked) => !cheked,
+    false
+  );
 
   return (
     <>
-    <h1> Current emotion is {emotion} and {secondary} </h1>
+      <input 
+        type="checkbox" 
+        value={cheked}
+        onChange={toggle}
+      />
 
-    <button onClick={() => setEmotion("Happy")}> 
-      Make Happy
-    </button>
-
-    <button onClick={() => setEmotion("Frustrated")}> 
-      Frustrate
-    </button>
-    
-    <button onClick={() => setEmotion("Enthusiastic")}> 
-      Enthuse
-    </button>
-
-    <button onClick={() => setSecondary("Crabby")}> 
-      Make Crabby
-    </button>
+      <p>
+        {cheked ? "cheked" : "not checked"}
+      </p>
     </>
   )
 }
