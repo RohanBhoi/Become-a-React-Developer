2e894ed8d4625dedbac00384b9c7698302f6f390
Rohan S Bhoi <s3764268@student.rmit.edu.au>
2021-11-08 15:52:47 +1100

    Working with useEffect

--- 1.React.js Essential Training/3.React State in a Component Tree/react-essentials/src/App.js
+++ 1.React.js Essential Training/3.React State in a Component Tree/react-essentials/src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
   const [emotion, setEmotion] = useState("happy");
-  console.log(emotion);
+  const [secondary, setSecondary] = useState("tired");
+
+  useEffect(() => {
+    console.log(`Its ${emotion} around here!`);
+  }, [emotion]);
+
+  useEffect(() => {
+    console.log(`Its ${secondary} around here!`);
+  }, [secondary]);
 
   return (
     <>
-    <h1> Current emotion is {emotion} </h1>
+    <h1> Current emotion is {emotion} and {secondary} </h1>
+
+    <button onClick={() => setEmotion("Happy")}> 
+      Make Happy
+    </button>
 
     <button onClick={() => setEmotion("Frustrated")}> 
       Frustrate
@@ -16,8 +28,13 @@ function App() {
     <button onClick={() => setEmotion("Enthusiastic")}> 
       Enthuse
     </button>
+
+    <button onClick={() => setSecondary("Crabby")}> 
+      Make Crabby
+    </button>
     </>
   )
 }
 
 export default App;
+
