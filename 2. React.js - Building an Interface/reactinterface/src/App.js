3e7c04d20147ba92a81837a42d0266f330bc4640
Rohan S Bhoi <rohanbhoi91@gmail.com>
2022-09-10 17:16:03 +0930

    Searching with a filtered array

--- 2. React.js - Building an Interface/reactinterface/src/App.js
+++ 2. React.js - Building an Interface/reactinterface/src/App.js
@@ -6,6 +6,17 @@ import AppointmentInfo from "./components/AppointmentInfo"
 
 function App() {
   let[appointmentList, setAppointmentList] = useState([]);
+  let[query, setQuery] = useState("");
+
+  const filteredAppointments = appointmentList.filter(
+    item => {
+      return(
+        item.petName.toLowerCase().includes(query.toLowerCase()) ||
+        item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+        item.aptNotes.toLowerCase().includes(query.toLowerCase())
+      )
+    }
+  )
 
   const fetchData = useCallback(() => {
     fetch('./data.json')
@@ -27,10 +38,11 @@ function App() {
 
       <AddAppointment/>
 
-      <Search/>
+      <Search query={query}
+      onQueryChange={myQuery => setQuery(myQuery)} />
 
       <ul className="divide-y divide-gre-200">
-        {appointmentList
+        {filteredAppointments
           .map(appointment => (
             <AppointmentInfo key={appointment.id} 
               appointment = {appointment}
